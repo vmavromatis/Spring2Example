24f54afa06844f2b3c9df321bfeb292de11aa54e
Thelgis Kotsos <thelgis@gmail.com>
2017-12-03 19:11:05 +0200

    Create application using Spring Boot Initializr with Gradle, Kotlin, and SpringBoot 2

---
+++ src/main/kotlin/com/thelgis/springexample/SpringexampleApplication.kt
@@ -0,0 +1,11 @@
+package com.thelgis.springexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringexampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringexampleApplication>(*args)
+}
